994baa5fe61324902dd3c231535aedb09301d52b
mohanra-fortude <mohana@fortude.co>
2021-08-17 10:36:13 +0530

    service tests

--- nest-crud/src/student/student.service.spec.ts
+++ nest-crud/src/student/student.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateStudentInput } from './dto/create-student.input';
+import { UpdateStudentInput } from './dto/update-student.input';
 import { StudentService } from './student.service';
 
 describe('StudentService', () => {
@@ -15,8 +16,7 @@ describe('StudentService', () => {
         },
       };
     }),
-
-  }
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -30,7 +30,7 @@ describe('StudentService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should create student', async() => {
+  it('should create student', async () => {
     const entry: CreateStudentInput = {
       id: 2,
       name: 'student',
@@ -38,13 +38,50 @@ describe('StudentService', () => {
       dob: '2000-03-03',
     };
 
-    const result = service.create(entry).catch((err)=>console.log(err))
+    const studentSpy = jest.spyOn(service, 'create');
+    service.create(entry);
+    expect(studentSpy).toHaveBeenCalledWith(entry);
+  });
 
-    jest.spyOn(service, 'create').mockImplementation(() => result);
+  it('should create students', async () => {
+    const entry: CreateStudentInput[] = [
+      {
+        id: 2,
+        name: 'student',
+        email: 'student@g.com',
+        dob: '2000-03-03',
+      },
+    ];
 
-  })
+    const studentSpy = jest.spyOn(service, 'createStudents');
+    service.createStudents(entry);
+    expect(studentSpy).toHaveBeenCalledWith(entry);
+  });
 
-  
-  
-    
+  it('should delete student', async () => {
+    const entry = 2;
+
+    const studentSpy = jest.spyOn(service, 'remove');
+    service.remove(entry);
+    expect(studentSpy).toHaveBeenCalledWith(entry);
+  });
+
+  it('should update student', async () => {
+    const entry: UpdateStudentInput = {
+      id: 2,
+      name: 'student',
+      email: 'student@g.com',
+      dob: '2000-03-03',
+    };
+
+    const studentSpy = jest.spyOn(service, 'update');
+    service.update(entry.id, entry);
+    expect(studentSpy).toHaveBeenCalledWith(entry.id, entry);
+  });
+
+  it('should get student', async () => {
+    const studentSpy = jest.spyOn(service, 'findAll');
+    service.findAll();
+    expect(studentSpy).toHaveBeenCalled();
+  });
 });
